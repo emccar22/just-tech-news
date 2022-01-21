040e30fdae122986097b4d0bd0c583e9396c36b7
emccar22 <e.mccarthy2285@gmail.com>
2022-01-20 23:10:55 -0500

    fix route

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 // import the Sequelize constructor from the library
-const { SequelizeScopeError } = require('sequelize');
+
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
 let sequelize;
-if(process.env.JAWDB_URL) {
-    sequelize = new Sequelize(process.env.JAWDB_URL);
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});
+  });
 }
-
 module.exports = sequelize;
