6a6c0272410c0486b1e89340c40c722f9166be23
emccar22 <e.mccarthy2285@gmail.com>
2022-01-20 22:23:41 -0500

    add comment model and comment routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -125,7 +125,7 @@ router.delete('/:id', (req, res) => {
         res.json(dbPostData);
     })
     .catch(err => {
-        console,log(err);
+        console.log(err);
         res.status(500).json(err);
     });
 });
