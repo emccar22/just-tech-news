972bb00b61cb64b6af3f7b4ec3bb26c5ed852494
emccar22 <e.mccarthy2285@gmail.com>
2022-01-30 16:13:35 -0500

    add edit page

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,4 @@
 const { Model, DataTypes } = require('sequelize');
-const { User, Post } = require('.');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {}
@@ -7,33 +6,31 @@ class Comment extends Model {}
 Comment.init(
   {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     comment_text: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [4]
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
     },
     user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
     },
     post_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'post',
-            key: 'id'
-        }
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
     }
   },
   {
